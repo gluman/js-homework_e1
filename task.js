c26f07d1d3455f393ff6ef14db74a5b2af8f5c33
Andrey Glumov <gluman@yandex.ru>
2025-02-23 18:56:39 +0300

    first commit today

--- task.js
+++ task.js
@@ -1,8 +1,22 @@
 const href = document.querySelectorAll('a')
 
-console(href)
-href.array.forEach((a)=> {
-    a.addEventListener('click', () => {
-        a.classlist.add('tooltip')
+//  console.log(href)
+
+href.forEach((a)=> {
+    
+    let tooltip
+    a.addEventListener('click', (event) => {
+        event.preventDefault()
+        let title_tooltip = a.title
+        let element = document.createElement('div')
+        if (tooltip) tooltip.remove();
+        element.classList.add('tooltip_active')
+        element.textContent = title_tooltip
+        element.classList.add('tooltip')
+        console.log(element)
+        a.insertAdjacentElement("beforeend", element )   
+    })
+    a.addEventListener('mouseleave', (event) => {
+        if (tooltip) tooltip.remove();
     })
 });
