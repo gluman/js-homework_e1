86379c75d7c7dce855b19adfe7f9ce32b3f4a675
Andrey Glumov <gluman@yandex.ru>
2025-02-23 20:53:18 +0300

    final commit today

--- task.js
+++ task.js
@@ -1,18 +1,40 @@
 const href = document.querySelectorAll('a');
+const positions = ['top', 'left', 'right', 'bottom'];
 
 href.forEach((a) => {
-    let tooltipElement = null; // Переменная для хранения элемента подсказки
-
+    let tooltipElement = null; 
+    const randomPosition = positions[Math.floor(Math.random() * positions.length)];
     a.addEventListener('click', (event) => {
         event.preventDefault();
-        
+        console.log(randomPosition)
         if (tooltipElement) {
             tooltipElement.remove();
         }
-
+        let rect = a.getBoundingClientRect()
         const title_tooltip = a.title;
-        tooltipElement = document.createElement('span'); 
-        tooltipElement.className = 'tooltip tooltip_active'; 
+        tooltipElement = document.createElement('div'); 
+        tooltipElement.className = 'tooltip tooltip_active';
+        if(randomPosition === 'top'){
+            tooltipElement.style.bottom = `${rect.top}px`;
+            tooltipElement.style.left = `${rect.left}px`;
+        }
+        else if(randomPosition === 'bottom'){
+            tooltipElement.style.top = `${rect.bottom - 2}px`;
+            tooltipElement.style.left = `${rect.left}px`;
+        }
+        else if(randomPosition === 'left'){
+            tooltipElement.style.top = `${rect.top}px`;
+            tooltipElement.style.right = `${rect.left - 2}px`;
+        }
+        else if(randomPosition === 'right'){
+            tooltipElement.style.top = `${rect.top}px`;
+            tooltipElement.style.left = `${rect.right + 2}px`;
+        }
+        else {
+            console.log('error if-else')
+        }
+        console.log (tooltipElement.style.top, tooltipElement.style.left)
+
         tooltipElement.textContent = title_tooltip;
         a.insertAdjacentElement("beforeend", tooltipElement);
     });
