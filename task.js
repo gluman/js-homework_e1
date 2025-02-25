9cd80e58085677446410b3665f32568263a8cca8
Andrey Glumov <gluman@yandex.ru>
2025-02-25 20:36:26 +0300

    some fix today

--- task.js
+++ task.js
@@ -1,48 +1,43 @@
 const href = document.querySelectorAll('a');
-const positions = ['top', 'left', 'right', 'bottom'];
 
 href.forEach((a) => {
-    let tooltipElement = null; 
-    const randomPosition = positions[Math.floor(Math.random() * positions.length)];
+    let tooltipElement = document.getElementsByClassName('tooltip_active')
+    const tooltipPosition = a.dataset.position
     a.addEventListener('click', (event) => {
-        event.preventDefault();
-        console.log(randomPosition)
-        if (tooltipElement) {
-            tooltipElement.remove();
-        }
+        event.preventDefault()
         let rect = a.getBoundingClientRect()
-        const title_tooltip = a.title;
-        tooltipElement = document.createElement('div'); 
-        tooltipElement.className = 'tooltip tooltip_active';
-        if(randomPosition === 'top'){
-            tooltipElement.style.bottom = `${rect.top}px`;
-            tooltipElement.style.left = `${rect.left}px`;
+        const title_tooltip = a.title
+        tooltipElement = document.createElement('div')
+        tooltipElement.className = 'tooltip_active tooltip_active'
+
+        if (tooltipPosition === 'top') {
+            tooltipElement.style.bottom = `${rect.top}px`
+            tooltipElement.style.left = `${rect.left}px`
         }
-        else if(randomPosition === 'bottom'){
-            tooltipElement.style.top = `${rect.bottom - 2}px`;
-            tooltipElement.style.left = `${rect.left}px`;
+        else if (tooltipPosition === 'bottom') {
+            tooltipElement.style.top = `${rect.bottom - 2}px`
+            tooltipElement.style.left = `${rect.left}px`
         }
-        else if(randomPosition === 'left'){
-            tooltipElement.style.top = `${rect.top}px`;
-            tooltipElement.style.right = `${rect.left - 2}px`;
+        else if (tooltipPosition === 'left') {
+            tooltipElement.style.top = `${rect.top}px`
+            tooltipElement.style.right = `${rect.left - 2}px`
         }
-        else if(randomPosition === 'right'){
-            tooltipElement.style.top = `${rect.top}px`;
-            tooltipElement.style.left = `${rect.right + 2}px`;
+        else if (tooltipPosition === 'right') {
+            tooltipElement.style.top = `${rect.top}px`
+            tooltipElement.style.left = `${rect.right + 2}px`
         }
         else {
             console.log('error if-else')
         }
-        console.log (tooltipElement.style.top, tooltipElement.style.left)
-
+        console.log(tooltipElement.style.top, tooltipElement.style.left)
         tooltipElement.textContent = title_tooltip;
         a.insertAdjacentElement("beforeend", tooltipElement);
     });
 
-    a.addEventListener('mouseleave', () => {
-        if (tooltipElement) {
+    a.addEventListener('click', () => {
+        if (tooltipElement.className.trim() !== '') {
             tooltipElement.remove();
-            tooltipElement = null; 
+            tooltipElement = null;
         }
     });
 });
